e356a8d4f69b12cf9fdd4c8451f06af4fdb6f04d
Remco Blom <rblom@sogyo.nl>
2017-03-06 10:54:47 +0100

    first part of Lists exercise - made two classes, one class has two methods for filling and printing array

---
+++ ListsLauncher.java
@@ -0,0 +1,43 @@
+/*
+	ListsLauncher.java
+	
+	Made by Remco Blom
+*/
+
+import java.lang.Math;
+
+/*
+	Starts the program
+*/
+public class ListsLauncher {
+	public static void main (String[] args) {
+		TenIntegerArray tenIntArr = new TenIntegerArray();
+		tenIntArr.fill();
+		tenIntArr.print();
+	}
+}
+
+/*
+	Makes an array of 10 places to fill with integers
+*/
+class TenIntegerArray {
+	int[] numList = new int[10];
+	int listLength = numList.length;
+	
+	public void fill() {
+		for (int i = 0; i < listLength; i++) {
+			numList[i] = (int) (Math.random() * 101);
+		}
+	}
+	
+	public void print() {
+		for (int i = 0; i < listLength; i++) {
+			System.out.print(numList[i]);
+			if (i != listLength - 1) {
+				System.out.print(", ");
+			} else {
+				System.out.println();
+			}
+		}
+	}
+}
