351d1cfceaeb0bada7a26acf359d94f78b91fc9f
Remco Blom <rblom@sogyo.nl>
2017-03-07 11:26:20 +0100

    made changes by feedback of Tos: changed the int array into arraylist

--- Prime.java
+++ Prime.java
@@ -3,33 +3,31 @@
 	
 	Made by Remco Blom
 */
-import java.util.Arrays;
+
+import java.util.ArrayList;
 
 public class Prime {
 	
 	public static void main (String[] args) {
+		int findPrime = 10001;
 		int counter = 2;
-		int primeCounter = 1;
-		int[] primes = {2};
-		System.out.println(primeCounter + " : " + counter);
-		
-		int primeNumber = 10001;
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		primes.add(2);
+		System.out.println(primes.size() + " : " + counter);
 		
 		// search for prime numbers until prime number 10001 is found
-		while (primeCounter != primeNumber) {
-			counter += 1;
+		while (primes.size() != findPrime) {
+			counter++;
 			boolean isPrime = true;
-			for (int prime : primes) {
+			for (Integer prime : primes) {
 				if (counter % prime == 0) {
 					isPrime = false;
 					break;
 				}
 			}
 			if (isPrime) {
-				primeCounter += 1;
-				primes = Arrays.copyOf(primes, primeCounter);
-				primes[primes.length - 1] = counter;
-				System.out.println(primeCounter + " : " + counter);
+				primes.add(counter);
+				System.out.println(primes.size() + " : " + counter);
 			}
 		}
 	}
