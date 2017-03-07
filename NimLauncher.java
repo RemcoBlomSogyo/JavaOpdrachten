bb62889e7b86b4e8e18f39e9b9ae76c1e84df356
Remco Blom <rblom@sogyo.nl>
2017-03-07 15:40:23 +0100

    finished second part of exercise. Added method computerTurn besides playerTurn.

--- NimLauncher.java
+++ NimLauncher.java
@@ -10,13 +10,14 @@ import java.util.InputMismatchException;
 public class NimLauncher {
 	public static void main (String[] args) {
 		NimGame nimGame = new NimGame();
-		nimGame.playTurn();
+		nimGame.choosePlayers();
 	}
 }
 
 class NimGame {
 	private int matches = 11;
 	private int turnPlayer = 1;
+	private int players = 0;
 	
 	public int getMatches() {
 		return matches;
@@ -34,6 +35,14 @@ class NimGame {
 		turnPlayer = turnPlr;
 	}
 	
+	public int getPlayers() {
+		return players;
+	}
+	
+	public void setPlayers(int plrs) {
+		players = plrs;
+	}
+	
 	public void changeTurnPlayer(int player){
 		if (player == 1) {
 			player++;
@@ -43,43 +52,82 @@ class NimGame {
 		setTurnPlayer(player);
 	}
 	
+	public void choosePlayers() {
+		System.out.print("Choose the amount of players (1/2): ");
+		
+		Scanner scan = new Scanner(System.in);
+		try {
+			setPlayers(scan.nextInt());
+		} catch (InputMismatchException e) {
+			System.out.println("That is not an integer.\n");
+			choosePlayers();
+		}
+		
+		if (getPlayers() != 1 && getPlayers() != 2) {
+			System.out.println("Choose between 1 or 2.\n");
+			choosePlayers();
+		}
+		
+		playerTurn();
+	}
+	
 	public void checkEndGame() {
 		if (getMatches() == 0) {
-			System.out.println("Player " + getTurnPlayer() + " took the last match.");
-			System.out.println("Player " + getTurnPlayer() + " lost.");
+			if (getPlayers() == 1 && getTurnPlayer() == 2) {
+				System.out.println("The computer took the last match.");
+				System.out.println("The computer lost.");
+			} else {
+				System.out.println("Player " + getTurnPlayer() + " took the last match.");
+				System.out.println("Player " + getTurnPlayer() + " lost.");
+			}
 		} else {
 			changeTurnPlayer(getTurnPlayer());
-			playTurn();
+			if (getPlayers() == 1 && getTurnPlayer() == 2) {
+				computerTurn();
+			} else {
+				playerTurn();
+			}
 		}
 	}
 	
-	public void playTurn() {
+	public void playerTurn() {
 		int inputNum = 0;
-		boolean isInt = false;
 		
-		while (!isInt) {
-			if (getMatches() == 1) {
-				System.out.println("There is " + getMatches() + " match.");
-			} else {
-				System.out.println("There are " + getMatches() + " matches.");
-			}
-			System.out.print("How many does player " + getTurnPlayer() + " want to take? ");
-
-			Scanner scan = new Scanner(System.in);
-			try {
-				inputNum = scan.nextInt();
-				isInt = true;
-			} catch (InputMismatchException e) {
-				System.out.println("That is not an integer.\n");
-			}
+		if (getMatches() == 1) {
+			System.out.println("There is " + getMatches() + " match.");
+		} else {
+			System.out.println("There are " + getMatches() + " matches.");
 		}
-		
+		System.out.print("How many does player " + getTurnPlayer() + " want to take? ");
+
+		Scanner scan = new Scanner(System.in);
+		try {
+			inputNum = scan.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("That is not an integer.\n");
+			playerTurn();
+			return;
+		}
+
 		if (checkValidInt(inputNum)) {
 			setMatches(getMatches() - inputNum);
 			checkEndGame();
 		} else {
-			playTurn();
+			playerTurn();
 		}
+	}
+	
+	public void computerTurn() {
+		if (getMatches() == 1) {
+			System.out.println("There is " + getMatches() + " match.");
+		} else {
+			System.out.println("There are " + getMatches() + " matches.");
+		}
+		System.out.print("How many does the computer want to take? ");
+		System.out.println("1\n");
+		
+		setMatches(getMatches() - 1);
+		checkEndGame();
 	}
 	
 	public boolean checkValidInt(int input) {
