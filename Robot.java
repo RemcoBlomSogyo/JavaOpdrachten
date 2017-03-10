687b7809e521315fc166267c62269afa35d7b8ea
Remco Blom <rblom@sogyo.nl>
2017-03-10 15:20:59 +0100

    finished last part of exercise

--- Robot.java
+++ Robot.java
@@ -4,10 +4,13 @@
 	Made by Remco Blom
 */
 
+import java.util.ArrayList;
+
 public class Robot {
 	private int posX = 0;
 	private int posY = 0;
 	private String facing = "north";
+	private ArrayList<String> commands = new ArrayList<String>();
 	
 	Robot(int x, int y, String face) {
 		posX = x;
@@ -15,28 +18,37 @@ public class Robot {
 		facing = face;
 	}
 	
+	// getters and setters
 	public int getPosX() {
 		return posX;
 	}
 	
-	public void setPosX(int x) {
-		posX = x;
+	public void setPosX(int newPosX) {
+		posX = newPosX;
 	}
 	
 	public int getPosY() {
 		return posY;
 	}
 	
-	public void setPosY(int y) {
-		posY = y;
+	public void setPosY(int newPosY) {
+		posY = newPosY;
 	}
 	
 	public String getFacing() {
 		return facing;
 	}
 	
-	public void setFacing(String face) {
-		facing = face;
+	public void setFacing(String newFacing) {
+		facing = newFacing;
+	}
+	
+	public ArrayList<String> getCommands() {
+		return commands;
+	}
+	
+	public void setCommands(ArrayList<String> newCommands) {
+		commands = newCommands;
 	}
 	
 	public void printState() {
@@ -44,7 +56,40 @@ public class Robot {
 			+ getPosY() + "), " + getFacing());
 	}
 	
-	public void turnLeft() {
+	// methods to fill the command list
+	public void goLeft() {
+		getCommands().add("left");
+	}
+	
+	public void goRight() {
+		getCommands().add("right");
+	}
+	
+	public void goForward(int speed) {
+		getCommands().add("forward" + speed);
+	}
+	
+	public void goBackward() {
+		getCommands().add("backward");
+	}
+	
+	public void execute() {
+		ArrayList<String> robotCommands = getCommands();
+		for (String command : robotCommands) {
+			if (command.equals("left")) {
+				turnLeft();
+			} else if (command.equals("right")) {
+				turnRight();
+			} else if (command.substring(0,7).equals("forward")) {
+				forward(Integer.parseInt(command.substring(7)));
+			} else {
+				backward();
+			}
+		}
+	}
+	
+	// action methods that execute method calls
+	private void turnLeft() {
 		if (getFacing().equals("north")) {
 			setFacing("west");
 		} else if (getFacing().equals("east")) {
@@ -56,7 +101,7 @@ public class Robot {
 		}
 	}
 	
-	public void turnRight() {
+	private void turnRight() {
 		if (getFacing().equals("north")) {
 			setFacing("east");
 		} else if (getFacing().equals("east")) {
@@ -68,11 +113,11 @@ public class Robot {
 		}
 	}
 	
-	public void forward() {
+	private void forward() {
 		forward(1);
 	}
 	
-	public void forward(int speed) {
+	private void forward(int speed) {
 		if (speed >= 1 && speed <= 3) {
 			if (getFacing().equals("north")) {
 				setPosY(getPosY() + speed);
@@ -86,7 +131,7 @@ public class Robot {
 		}
 	}
 	
-	public void backward() {
+	private void backward() {
 		if (getFacing().equals("north")) {
 			setPosY(getPosY() - 1);
 		} else if (getFacing().equals("east")) {
@@ -98,13 +143,16 @@ public class Robot {
 		}
 	}
 	
+	// main method that calls actions from other methods
 	public static void main(String[] args) {
 		Robot robin = new Robot(2, 4, "south");
 		Robot blitzCrank = new Robot(1, 2, "west");
-		robin.turnRight();
-		robin.forward(3);
-		blitzCrank.turnLeft();
-		blitzCrank.backward();
+		robin.goRight();
+		robin.goForward(3);
+		robin.execute();
+		blitzCrank.goLeft();
+		blitzCrank.goBackward();
+		blitzCrank.execute();
 		System.out.print("Robin: ");
 		robin.printState();
 		System.out.print("Blitzcrank: ");
