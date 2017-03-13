79293fd28c9db7d667370e9e9c9190c67db88d3a
Remco Blom <rblom@sogyo.nl>
2017-03-13 16:20:15 +0100

    fixed bug with args in launcher

--- LcdLauncher.java
+++ LcdLauncher.java
@@ -9,10 +9,14 @@ public class LcdLauncher {
 	public static void main(String[] args) {
 
 		Lcd lcd = new Lcd();
-		if (args[0].equals("-s") && args[1] != null) {
-			int size = Integer.parseInt(args[1]);
-			if (size >= 1 && size <= 5) {
-				lcd.start(size);
+		if (args.length == 2) {
+			if (args[0].equals("-s") && args[1] != null) {
+				int size = Integer.parseInt(args[1]);
+				if (size >= 1 && size <= 5) {
+					lcd.start(size);
+				} else {
+					lcd.start(2);
+				}
 			} else {
 				lcd.start(2);
 			}
